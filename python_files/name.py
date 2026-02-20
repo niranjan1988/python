de9e62427abd83aeb98b9edc8779909af4eda2d9
Niranjan <nvallivedu@dalet.com>
2026-02-20 17:46:27 +0530

    properties, comprehensions, special methods and functions

---
+++ python_files/name.py
@@ -0,0 +1,3 @@
+import sample
+
+print(__name__) # This prints __main__ when this file is run directly, and prints name when this file is imported as a module.
--- python_files/sample.py
+++ python_files/sample.py
@@ -1,2 +1,4 @@
 a=input("Enter name: ")
 print("Hello, " + a + "!")
+
+print(__name__) # This prints __main__ when this file is run directly, and prints sample when this file is imported as a module.
